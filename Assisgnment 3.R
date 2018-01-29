4fbcc99dda8c6209e9b762b8529ab7a50c892b9c
MsOshie <stephyoge@mail.usf.edu>
2018-01-28 21:07:31 -0500

    Update Assisgnment 3.R

--- Assisgnment 3.R
+++ Assisgnment 3.R
@@ -17,25 +17,33 @@ Name ABCPollResult NCBPollResult
 Warning message:
   In mean.default(combine.df) :
   argument is not numeric or logical: returning NA
-> mean(combine.df[,2:3])
-[1] NA
-Warning message:
-  In mean.default(combine.df[, 2:3]) :
-  argument is not numeric or logical: returning NA
-> mean(combine.df [,2:,3])
-Error: unexpected ',' in "mean(combine.df [,2:,"
-> mean(combine.df [,2:3])
-[1] NA
-Warning message:
-  In mean.default(combine.df[, 2:3]) :
-  argument is not numeric or logical: returning NA
-> colMeans(ABCPollResult, NCBPollResult)
-Error in colMeans(ABCPollResult, NCBPollResult) : 
-  'x' must be an array of at least two dimensions
-> C.df<-data.frame(a,b,c,d)
-Error in data.frame(a, b, c, d) : object 'a' not found
-> mean(combine.df[,2:3])
-[1] NA
-Warning message:
-  In mean.default(combine.df[, 2:3]) :
-  argument is not numeric or logical: returning NA
+
+
+#New trial of code using a different method!
+> Name = c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+> ABC_political = c(4, 62, 51, 21, 2, 14, 15)
+> NCB_political = c(12, 75, 43, 19, 1, 21, 19)
+> results = cbind(Name, ABC_political, NCB_political)
+> results
+     Name      ABC_political NCB_political
+[1,] "Jeb"     "4"           "12"         
+[2,] "Donald"  "62"          "75"         
+[3,] "Ted"     "51"          "43"         
+[4,] "Marco"   "21"          "19"         
+[5,] "Carly"   "2"           "1"          
+[6,] "Hillary" "14"          "21"         
+[7,] "Bernie"  "15"          "19"         
+> results.df = data.frame(Name, ABC_political, NCB_political)
+> results.df
+     Name ABC_political NCB_political
+1     Jeb             4            12
+2  Donald            62            75
+3     Ted            51            43
+4   Marco            21            19
+5   Carly             2             1
+6 Hillary            14            21
+7  Bernie            15            19
+> r=rowMeans(results.df[,2:3])
+> r.df=data.frame(Name,r)
+> mean(as.matrix(results.df[,2:3]))
+[1] 25.64286
