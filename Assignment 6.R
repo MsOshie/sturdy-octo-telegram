03ea3ee965f97de24a9bf295327847961427f22d
MsOshie <stephyoge@mail.usf.edu>
2018-02-18 09:35:11 -0500

    code for assignment 6

---
+++ Assignment 6.R
@@ -0,0 +1,17 @@
+A=matrix(c(2,0,1,3), ncol=2) 
+B=matrix(c(5,2,4,-1), ncol=2)
+
+A + B
+A - B
+
+help("diag")
+diag(A)
+
+N=matrix(c(4,1,2,3), ncol = 2)
+N
+nn <- diag(N)
+nn
+diag(nn)
+
+diag(5)
+diag(x=3, ncol = 5, nrow = 5)
